acc85cbd13b78e4b90b8bb1d537badfa91c55d1d
Pascal Jarbath <30990904+PeaceWise@users.noreply.github.com>
2024-09-19 15:43:11 -0400

    Added route to view survey outcomes, and the page to view them.

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -1,4 +1,6 @@
 class SurveysController < ApplicationController
+
+ #This is for the new survey form
   def new
     @survey = Survey.new
   end
@@ -9,8 +11,15 @@ class SurveysController < ApplicationController
       redirect_to new_survey_path, notice: "Thank you for submitting the survey!"
     else
       render :new, alert: "There was a problem with your submission."
-    end
+    end  
   end
+
+#This is for the viewing page for all of the survey submissions.
+  def index
+    @surveys = Survey.all
+  end
+  
+
 
   private
 
